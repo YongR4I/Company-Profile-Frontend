86d826572124ac0261dba927321c0ce1deee17e6
YongR4I <raihandaffa19072009@gmail.com>
2026-07-07 20:48:21 +0700

    feat: implement portfolio footer, partner, and cta section components

--- src/components/sections/portfolio/footer/index.tsx
+++ src/components/sections/portfolio/footer/index.tsx
@@ -1,11 +1,5 @@
-import CommonLayout from "@/components/layout/CommonLayout";
+import FooterComponent from "@/components/layout/Footer";
 
 export default function Footer() {
-  return (
-    <CommonLayout className="items-center justify-center !bg-slate-900">
-      <h1 className="text-white font-bold tracking-tighter leading-[0.9] text-[clamp(40px,8vw,100px)]">
-        FOOTER
-      </h1>
-    </CommonLayout>
-  );
+  return <FooterComponent />;
 }
--- src/components/sections/portfolio/partner/index.tsx
+++ src/components/sections/portfolio/partner/index.tsx
@@ -1,11 +1,5 @@
-import CommonLayout from "@/components/layout/CommonLayout";
+import Partners from "@/components/sections/Partners";
 
 export default function Partner() {
-  return (
-    <CommonLayout className="items-center justify-center !bg-red-600">
-      <h1 className="text-white font-bold tracking-tighter leading-[0.9] text-[clamp(40px,8vw,100px)]">
-        PARTNER
-      </h1>
-    </CommonLayout>
-  );
+  return <Partners />;
 }
