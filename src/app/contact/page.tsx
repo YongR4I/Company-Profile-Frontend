c93d1d1b90c273e4cd9098bc73ab1de5f96c3067
YongR4I <raihandaffa19072009@gmail.com>
2026-07-08 11:13:13 +0700

    add footer to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
 import ContactHero from "@/components/sections/contact/hero";
 import ContactForm from "@/components/sections/contact/contactform";
+import Footer from "@/components/layout/Footer";
 
 export default function ContactPage() {
   return (
     <div className="flex flex-col min-h-screen bg-water-900">
       <ContactHero />
       <ContactForm />
+      <Footer />
     </div>
   );
 }
