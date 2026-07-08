6249e94e371859f2755eb651ce7525b687d3e2f6
AuzaAl <auzaganteng007@gmail.com>
2026-07-08 13:12:15 +0700

    fix: update duration in LenisProvider for smoother scrolling

--- src/components/layout/LenisProvider.tsx
+++ src/components/layout/LenisProvider.tsx
@@ -6,7 +6,7 @@ import { type ReactNode } from "react";
 export default function LenisProvider({ children }: { children: ReactNode }) {
   return (
     <ReactLenis root options={{
-      duration: 0.8,
+      duration: 0.3,
       easing: (t) => 1 - Math.pow(1 - t, 3),
       wheelMultiplier: 0.8,
       touchMultiplier: 0.8,
